7c6705c649f134c7e81c45094a3b9f2f0cfaf4f0
Danny Lowater <dannylow456@gmail.com>
2021-07-02 15:37:53 +1000

    add create-react-app

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>buildkite test baby</h1>
+      <Image
+        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDAwIDY2NyI+PHBhdGggZmlsbD0iIzMwRjJBMiIgZD0iTTAgMGwzMzMuMyAxNjUuM3YzMzMuNEwwIDMzMy4zTTY2OS4zIDBMMTAwMCAxNjUuM2wtMzMwLjcgMTY4Ii8+PHBhdGggZmlsbD0iIzE0Q0M4MCIgZD0iTTY2OS4zIDBsLTMzNiAxNjUuM3YzMzMuNGwzMzYtMTY1LjRtMzMwLjctMTY4bC0zMzAuNyAxNjh2MzMzLjRsMzMwLjctMTY4Ii8+PC9zdmc+"
+        width="500"
+        height="500"
+        alt="buildkite logo"
+      />
+    </div>
+  );
+}
